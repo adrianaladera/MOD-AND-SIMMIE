38697bc73c550d7a67491a9337ab37e14bb68c9d
Alex <alexpaul.koenig@gmail.com>
2022-11-22 14:01:29 -0500

    dynamic trapezoidal

--- trapezoidal.jl
+++ trapezoidal.jl
@@ -63,3 +63,42 @@ function trapezoidal(f, x0, xn, y0, n)
     
     return ys
 end
+
+
+function trapezoidal_dynamic(f, x0, xn, y0, n)
+    # Use the trapezoidal method to propagate the orbits
+    # x0: initial time
+    # xn: final time
+    # n: number of steps
+    # y0: initial state
+
+    h = (xn - x0)/n  # time step
+    xs = zeros(floor(Int,n+1))
+    ys = zeros(floor(Int,n*2),length(y0))
+    xs[1] = x0
+    ys[1,:] = y0
+    
+    # step: nonlinear solve (for x1) of x1 - x0 - dt/2 f(x1) - dt/2 f(x0)
+    function step(x1, x0, h)
+    	return x1 - x0 - h/2 * f(x1) - h/2 * f(x0)
+    end
+    t = 0
+    i = 1
+    while t<xn
+        t = t + h
+        xs[i] = t
+
+        ys[i+1,:] = NewtonNd(step, ys[i,:], ys[i,:], h, 1e-9, 1e-8, 1e-8, 10000)
+        # (Initial guess for ys[i+1] is ys[i])
+        println(norm(ys[i+1,:]-ys[i,:]))
+        if norm(ys[i+1,:]-ys[i,:]) >= 1e6
+                h = h*0.9
+        end
+        if norm(ys[i+1,:]-ys[i,:]) <= 1e5
+            h = h*1.1
+        end
+        i = i + 1
+        
+    end    
+    return ys,xs
+end
