5d104da0f5fa0267a148798239bc00710e19463e
adrianaladera <70233099+adrianaladera@users.noreply.github.com>
2022-11-22 11:26:16 -0500

    ayuda me

---
+++ .ipynb_checkpoints/forward_euler-checkpoint.jl
@@ -0,0 +1,22 @@
+using LinearAlgebra
+
+function euler(f, x0, xn, y0, n)
+    # Use the forward euler method to propagate the orbits
+    # x0: initial time
+    # xn: final time
+    # n: number of steps
+    # y0: initial state
+
+    h = (xn - x0)/n  # time step
+    xs = zeros(floor(Int,n+1))
+    ys = zeros(floor(Int,n+1),length(y0))
+    xs[1] = x0
+    ys[1,:] = y0
+    
+    for i in 1:floor(Int,n)
+        xs[i + 1] = xs[i] + h
+        ys[i + 1,:] = ys[i,:] + h * f(ys[i,:])
+    end
+    
+    return ys
+end
