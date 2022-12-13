7849a2e7ab97da91b695a480f3fca320ece436ce
Alex <alexpaul.koenig@gmail.com>
2022-12-12 22:07:14 -0500

    run_system.jl prep

--- run_system.jl
+++ run_system.jl
@@ -1,35 +1,36 @@
-using LinearAlgebra, Distributions, Random, Rotations
+using LinearAlgebra
 using Printf
-using Plots
+using DelimitedFiles
 
 global const µ = 3.986e14  # m^3/s^2
 
 include("eval_f.jl")
 include("forward_euler.jl")
-include("trapezoidal.jl")
+# include("trapezoidal.jl")
 include("generate_random_initial_state.jl")
-include("visualizeNetwork.jl")
+# include("visualizeNetwork.jl")
 
-function f(x)
-    feval(x,0,0)
-end
+f(x) = feval(x,0,0)
 
-mean_a = 1e7
+mean_a = 8.5e6
 stddev_a = 1e5
 mean_e = 0.01
-stddev_e = 0.2
+stddev_e = 0.1
 
-N = 200
+N = 1000
 
-x0 = generate_random_initial_state(N, mean_a, stddev_a, mean_e, stddev_e, true);
+x0 = generate_random_initial_state(N, mean_a, stddev_a, mean_e, stddev_e, true)
 
-orbits = 5
-t0 = 0
-ti = 8e3 * orbits
-num_steps = 5000 * orbits
-
-xs = euler(f, t0, ti, x0, num_steps);
-
+orbits = 10
+for i in 1:orbits
+    t0 = 0
+    ti = 8e3
+    num_steps = 20000
+    xs = euler(f, t0, ti, x0, num_steps)
+    global x0 = xs[end,:]
+    writedlm("simulation_20000_$(N)objects_$(orbits)orbit_$(i).txt", Float32.(xs[1:40:end-1,:]))
+    println("completed orbit $(i)")
+end
 # before you run this, check that this isn't going to overwrite another file
-using DelimitedFiles
-writedlm("simulation_$(N)objects_$(orbits)orbits.txt", xs)
+
+
